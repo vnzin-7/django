1124bcf0ea8843de81b79af14b62976c60b05942
vnzin-7 <viniciulaika98@gmail.com>
2025-06-27 16:33:11 -0300

    vnzin-7

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index),
+    path('', views.pagina1, name='pagina1'),
+    path('pagina2/', views.pagina2, name='pagina2'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, "app/index.html")
     return render(request,"tindex.html")
+
+def pagina1(request):
+    return render(request, 'pagina1.html')
+
+def pagina2(request):
+    return render(request, 'pagina2.html')
